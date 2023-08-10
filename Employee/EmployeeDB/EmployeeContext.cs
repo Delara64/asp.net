8b57ebdd48b18428c8f41c6969680b3f944827d7
DELARA <delara.amiri86@gmail.com>
2023-08-10 15:36:51 -0400

    no message

---
+++ Employee/EmployeeDB/EmployeeContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeDB
+{
+  
+        public class EmployeeContext
+        {
+        private EmployeeEntities _Ctx;
+
+            public EmployeeContext()
+            {
+                _Ctx = new EmployeeEntities();
+            }
+
+            public EmployeeEntities Context
+            {
+                get
+                {
+                    return this._Ctx;
+                }
+            }
+
+            public void Dispose()
+            {
+                if (_Ctx != null)
+                    _Ctx.Dispose();
+            }
+        }
+    }
---
+++ Employee/EmployeeDB/EmployeeRepository.cs
@@ -0,0 +1,36 @@
+﻿using EmployeeDB.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeDB
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private EmployeeContext _Ctx;
+
+        public EmployeeRepository(EmployeeContext Context)
+        {
+            _Ctx = Context;
+            _Ctx.Context.Configuration.LazyLoadingEnabled = false;
+            _Ctx.Context.Configuration.ProxyCreationEnabled = false;
+
+
+        }
+        //Start Method development
+        public IQueryable<Employee> GetEmployee()
+        {
+            return _Ctx.Context.Employees;
+        }
+
+        //End Method development
+
+        public void Dispose()
+        {
+            if (_Ctx != null)
+                _Ctx.Dispose();
+        }
+    }
+}
